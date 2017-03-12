a6fff559acd64a5b1bd0f7aa75fb187d7022ddbd
emoto a <s1620606@u.tsukuba.ac.jp>
2017-03-12 09:11:20 +0000

    fix

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,12 +1,13 @@
 class RankingController < ApplicationController
-<<<<<<< HEAD
-=======
+
   def want
+     item_ids = Want.group(:item_id).order('count_all desc').limit(10).count.keys
+     @items = Item.find(item_ids).sort_by{|item| item_ids.index(item.id)}
   end
 
   def have
      item_ids = Have.group(:item_id).order('count_all desc').limit(10).count.keys
      @items = Item.find(item_ids).sort_by{|item| item_ids.index(item.id)}
   end
->>>>>>> user_item
+
 end
